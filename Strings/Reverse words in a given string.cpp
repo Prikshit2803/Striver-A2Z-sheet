83d4498278f100b083b4c9a0abab44aae08bf2ae
Prikshit2803 <119496129+Prikshit2803@users.noreply.github.com>
2023-08-22 00:03:07 +0530

    Update Reverse words in a given string.cpp

--- Strings/Reverse words in a given string.cpp
+++ Strings/Reverse words in a given string.cpp
@@ -26,3 +26,32 @@
         
         return ans;
     } 
+
+//leetcode version : spaces are there and not dots and remove initial and ending unecesarry spaces
+tring reverseWords(string s) {
+       
+       string temp="",ans="";
+
+       int i=0;
+
+       while(i<s.length()){
+
+           if(s[i]!=' '){
+               temp+=s[i];
+           }
+
+           else if(s[i]==' ' && temp!=""){ //space encountered and temp not null i.e it indeicates a space after a word
+                   ans=temp + " " + ans; //reverses words
+                   temp=""; //make temp NULL
+           }
+
+          
+          i++;
+       }
+
+       if(temp!=""){ //indicates no space after last word
+             ans=temp + " " + ans;
+       }
+
+       return ans.substr(0,ans.length()-1);
+    }
